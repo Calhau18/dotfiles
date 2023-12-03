e618bb2feef63b1a6abb13a148489256c4c589b0
João Rocha <joao.tfrocha@protonmail.com>
2023-12-03 18:52:15 +0100

    Splitting polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+polybar-msg cmd quit
+
+polybar background --reload 2>&1 & disown
+polybar left --reload 2>&1 & disown
+polybar center --reload 2>&1 & disown
+polybar right --reload 2>&1 & disown
