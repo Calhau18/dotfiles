a3ae97831e7f9ed58d44cfba02fa5cd672fe2982
João Rocha <joao.tfrocha@protonmail.com>
2025-02-25 08:44:09 +0100

    Polybar: improve keyboard and create monitor modules

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,8 +5,8 @@ polybar-msg cmd quit
 
 if [[ "$@" == "" ]]; then
     polybar left --log=error --reload 2>&1 & disown
-    polybar center --log=error --reload 2>&1 & disown
     polybar right --log=error --reload 2>&1 & disown
+    polybar center --log=error --reload 2>&1 & disown
 else
     for bar in "$@"; do
 	polybar $bar --reload 2>&1 & disown
